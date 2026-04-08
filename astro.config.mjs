72cf0e7eb66a98798c75a50d6913db06708894a7
Tristan Trébaol <tristan.trebaol@gmail.com>
2026-04-08 20:48:29 +0200

    Use custom domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://tristantreb.github.io',
-  base: '/me',
+  site: 'https://tristan.trebaol.com',
+  base: '/',
 });
